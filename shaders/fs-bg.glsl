a54e8fceeebd832a3ba1407662b2a5d57aa2a4e6
cabbibo <isaaclandoncohen@gmail.com>
2015-09-14 13:36:40 -0700

    working on transition

--- shaders/fs-bg.glsl
+++ shaders/fs-bg.glsl
@@ -7,21 +7,29 @@ uniform float parameter4;
 uniform float parameter5;
 uniform float parameter6;
 
+uniform vec3 lightColor1;
+uniform vec3 lightColor2;
+
+uniform float filledness;
+uniform float completed;
+
 varying vec3 vPos;
 varying vec3 vCam;
 varying vec3 vNorm;
 
-varying vec3 vLight;
+varying vec3 vLight1;
+varying vec3 vLight2;
+
 
 varying vec2 vUv;
 
 
 
-const float MAX_TRACE_DISTANCE = 40.;           // max trace distance
+const float MAX_TRACE_DISTANCE = 5.;           // max trace distance
 const float INTERSECTION_PRECISION = 0.1;        // precision of the intersection
-const int NUM_OF_TRACE_STEPS = 10;
+const int NUM_OF_TRACE_STEPS = 20;
 
-
+vec3 sunPos; 
 
 
 
@@ -74,6 +82,15 @@ float opRepBox( vec3 p, vec3 c , float r)
     return sdBox( q  ,vec3( r ));
 }
 
+vec2 smoothU( vec2 d1, vec2 d2, float k)
+{
+    float a = d1.x;
+    float b = d2.x;
+    float h = clamp(0.5+0.5*(b-a)/k, 0.0, 1.0);
+    return vec2( mix(b, a, h) - k*h*(1.0-h), mix(d2.y, d1.y, pow(h, 2.0)));
+}
+
+
 
 // Using SDF from IQ's two tweet shadertoy : 
 // https://www.shadertoy.com/view/MsfGzM
@@ -90,8 +107,8 @@ float sdBlob( vec3 p ){
 
 float sphereField( vec3 p ){
 
-  float fieldSize = 1.  + abs( sin( parameter5) ) * 1.;
-  return opRepSphere( p , vec3(fieldSize ), .01 + parameter4 * .05 );
+  float fieldSize = 2. - filledness; //abs( sin( pa) ) * 1.;
+  return opRepSphere( p , vec3(fieldSize ), .04 + parameter4 * .05 );
 
 }
 
@@ -111,30 +128,104 @@ float sdBlob2( vec3 p ){
 
 }
 
+
+mat4 rotateX(float angle){
+    
+  angle = -angle/180.0*3.1415926536;
+    float c = cos(angle);
+    float s = sin(angle);
+  return mat4(1.0, 0.0, 0.0, 0.0, 0.0, c, -s, 0.0, 0.0, s, c, 0.0, 0.0, 0.0, 0.0, 1.0);
+    
+}
+
+mat4 rotateY(float angle){
+    
+  angle = -angle/180.0*3.1415926536;
+    float c = cos(angle);
+    float s = sin(angle);
+  return mat4(c, 0.0, s, 0.0, 0.0, 1.0, 0.0, 0.0, -s, 0.0, c, 0.0, 0.0, 0.0, 0.0, 1.0);
+    
+}
+
+mat4 rotateZ(float angle){
+    
+  angle = -angle/180.0*3.1415926536;
+    float c = cos(angle);
+    float s = sin(angle);
+  return mat4(c, -s, 0.0, 0.0, s, c, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+    
+}
+
+mat4 translate(vec3 t){
+    
+  return mat4(1.0, 0.0, 0.0, -t.x, 0.0, 1.0, 0.0, -t.y, 0.0, 0.0, 1.0, -t.z, 0.0, 0.0, 0.0, 1.0);
+    
+}
+
+
+vec3 palette( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d )
+{
+    return a + b*cos( 6.28318*(c*t+d) );
+}
+
+float mandala( vec3 pos ){
+
+
+  vec4 p = vec4( pos.xyz , 1. );
+
+//vec3 nP = vec3( pos.x , mod( pos.y , .8 ) , pos.z )
+
+
+  vec2 res = vec2(10000.,10000.);
+
+  for( int i = 0; i < 8; i++ ){
+    float degrees = (float( i ) / 8.) * 360.;
+
+    mat4 m = rotateX(degrees) * translate(vec3(0.0,  completed * 2. , 0.0)) ;
+
+    p = vec4( pos.xyz , 1. ) * m; 
+    //p = vec4( p.x , mod( p.y , 1.) , p.z , 1.);
+
+    res = smoothU( res , vec2( length(p.xyz) - .4 , 1. ) , .2);
+
+
+
+  }
+
+ // degrees = mod( degrees , 3.14159 / 10.2 );
+
+ // vec3 nP = mod( p + vec3( .3 , .3 , .3 ), vec3( 1. , 1. , 1.) ); //vec3( sin( degrees ) * radius , cos( degrees ) * radius , 0. );//tan( degrees ) * radius;
+
+ 
+ //vec3 nP = vec3( p.x , mod( p.y , 1. ) , mod( p.z, .5 ) );
+
+  return res.x; //length(  nP ) - .3;
+
+
+}
+
 //--------------------------------
 // Modelling 
 //--------------------------------
 vec2 map( vec3 pos ){  
    
-    // using super thin cube as plane
-    vec3 size = vec3( 1.  , 1. , .01 );
-   // vec3 rot = vec3( iGlobalTime * .1 , iGlobalTime * .4 , -iGlobalTime * .3 );
-    vec3 rot = vec3( 0.,0.,0. );
-   // vec2 res = vec2( rotatedBox( pos , rot , size , .001 ) , 1.0 );
-    
-    float repSize = ( parameter1 * .4 + .4) * 2.;
-    repSize = 2.;
 
-    float radius = .4 * parameter2  + .1;
 
-    radius = .01;
+   //// vec2 res = vec2( opRepSphere( pos , vec3( repSize ) , radius ) , 1. );
+    vec2 res = vec2( sdSphere( pos - sunPos, .6) , 1. );
+    vec2 res2 = vec2( sdSphere( pos + vec3( 0., 20. , 0. ), 18. ) , 2. );
 
-   // vec2 res = vec2( opRepSphere( pos , vec3( repSize ) , radius ) , 1. );
-    //vec2 res = vec2( sdSphere( pos ,  radius ) , 1. );
-    //vec2 res = vec2( sdBlob( pos ) , 1. );
+    res = smoothU( res , res2 , .2 );
 
-    vec2 res =  vec2( sphereField( pos ) , 2. );
-    return res;
+    //res2 = vec2( mandala( pos - sunPos ) , 1. );
+
+    res = smoothU( res , res2 , .2 );
+
+
+    vec2 res3 =  vec2( sphereField( pos ) , 2. );
+
+
+    return res3 * completed + res * (1. - completed);
     
 }
 
@@ -181,63 +272,151 @@ vec3 calcNormal( in vec3 pos ){
 }
 
 
-vec3 doCol( float lamb , float spec ){
-
-  float nSpec= pow( spec , abs(sin(parameter1 * 1.1))* 10. + 2. );
-  return
-      hsv( lamb * .3 + parameter2 , abs( sin( parameter6 )) * .2 + .6 , abs( sin( parameter2 ) * .4 + .6 )) * lamb 
-    + hsv( nSpec * .6 + parameter3 , abs( sin( parameter5 )) * .4 + .6 , abs( sin( parameter1 ) * .3 + .8 )) * nSpec;
+float calcAO( in vec3 pos, in vec3 nor )
+{
+  float occ = 0.0;
+    float sca = 1.0;
+    for( int i=0; i<5; i++ )
+    {
+        float hr = 0.01 + 0.612*float(i)/4.0;
+        vec3 aopos =  nor * hr + pos;
+        float dd = map( aopos ).x;
+        occ += -(dd-hr)*sca;
+        sca *= 0.5;
+    }
+    return clamp( 1.0 - 3.0*occ, 0.0, 1.0 );    
 }
 
+
+vec2 doLight( vec3 lightPos , vec3 pos , vec3 norm , vec3 eyeDir ){
+
+  vec3 lightDir = normalize( lightPos - pos );
+  vec3 reflDir  = reflect( lightDir , norm );
+
+
+  float lamb = max( 0. , dot( lightDir , norm   ) );
+  float spec = max( 0. , dot( reflDir  , eyeDir ) );
+
+  return vec2( lamb , spec );
+
+}
+
+
+vec3 doBoxShading( vec2 l1 , vec2 l2 , vec3 ro ){
+
+  vec3 col = vec3( 0. );
+
+  float fillednessVal = (( ro.y + 1.5 )  / 3. ) * filledness;
+
+  vec3 a = vec3( .5 );
+  vec3 b = vec3( .5 );
+  vec3 c = vec3( 2. , 1.,  0. );
+  vec3 d = vec3( .5 , .2 , .25 );
+  float spec = pow( l1.y , 40. );
+  col +=  palette( l1.x + spec * 1., a , b , c ,d ) * (  spec ) * .5;
+
+  a = vec3( .5 );
+  b = vec3( .5 );
+  c = vec3( 1. , 1.,  0. );
+  d = vec3( .8 , .9 , .3 );
+  spec = pow( l2.y , 40. );
+  col +=  palette( l2.x + spec * 1., a , b , c ,d ) * (  spec ) * .5;
+
+
+  float edgeSize = .05  * (1. - completed ) + .01;
+  if( vUv.x < edgeSize || vUv.x > 1. - edgeSize || vUv.y < edgeSize || vUv.y > 1. - edgeSize ){
+    col += vec3( .3 , .3 , .3 );
+  }
+
+  return col;
+
+}
+
+vec3 doBackgroundShading( vec2 l1 , vec2 l2 , vec3 ro ){
+
+  float fillednessVal = ( ro.y + 1.5 ) / 3. * filledness * ( 1. + completed );
+
+
+  vec3 a = vec3( .5 );
+  vec3 b = vec3( .5 );
+  vec3 c = vec3( 1. , 1.,  0. );
+  vec3 d = vec3( .8 , .9 , .3 );
+
+  vec3 p =  palette( fillednessVal / 2., a , b , c ,d );
+
+  vec3 col = p  * filledness* ( 1. + completed * .4 ) * .5; //* ( 1. - completed );
+
+  return col;
+
+}
+
+
+vec3 doRayShading( vec2 l1 , vec2 l2  , vec3 norm , vec3 ro ){
+
+  vec3 col = vec3( 0. );
+
+  vec3 a = vec3( .5 );
+  vec3 b = vec3( .5 );
+  vec3 c = vec3( 2. , 1.,  0. );
+  vec3 d = vec3( .5 , .2 , .25 );
+  float spec = pow( l1.y , 10. );
+  col +=  palette( l1.x , a , b , c ,d ) * ( l1.x + spec );
+
+  a = vec3( .5 );
+  b = vec3( .5 );
+  c = vec3( 1. , 1.,  0. );
+  d = vec3( .8 , .9 , .3 );
+  spec = pow( l2.y , 10. );
+  col +=  palette( l2.x , a , b , c ,d ) * ( l2.x  + spec );
+
+  //col += norm * .5 + .5;
+
+  col += doBackgroundShading( l1 , l2 , ro ); //}
+
+  return col;
+}
 
 
 
 
 void main(){
 
+  sunPos = vec3( 0. , filledness * 2. - 3. + completed * 5. , -3.6 );
+
   vec3 ro = vPos;
   vec3 rd = normalize( vPos - vCam );
 
-  vec3 lightDir = normalize( vLight - ro);
-
   vec2 res = calcIntersection( ro , rd );
 
-  vec3 reflDir = reflect( lightDir , vNorm );
 
-  float lamb = max( dot( vNorm , lightDir), 0.);
-  float spec = max( dot( reflDir , rd ), 0.);
-
-
-  float iLamb = max( dot( -vNorm , lightDir), 0.);
-  vec3  iReflDir = reflect( lightDir , -vNorm );
-  float iSpec = max( dot( iReflDir , rd ), 0.);
+  vec2 light1 = doLight( vLight1 , ro , vNorm , rd );
+  vec2 light2 = doLight( vLight2 , ro , vNorm , rd );
 
 
-  vec3 col = doCol( iLamb , iSpec );//-vNorm * .5 + .5;
-  
+
+  float fillednessVal = (( ro.y + 1.5 )  / 3. ) * filledness;
+  vec3 col = vec3( 0. , 0. , 0. );
+
+  col += doBoxShading( light1 , light2 , ro );
+
   if( res.y > .5 ){
 
     vec3 pos = ro + rd * res.x;
-
-    vec3 lightDir = normalize( vLight - pos);
     vec3 norm = calcNormal( pos );
-    
-    vec3 reflDir = reflect( lightDir , norm );
-
-    float lamb = max( dot( norm , lightDir), 0.);
-    float spec = max( dot( reflDir , rd ), 0.);
 
 
-    //col = lamb * vec3( 1. , 0. , 0. ) + pow( spec , 10.) * vec3( 0. , 0. , 1. );// norm * .5 +.5;
-    col = doCol( lamb , spec );
+    light1 = doLight( vLight1 , pos , norm , rd );
+    light2 = doLight( vLight2 , pos , norm , rd );
+
+   // col += norm * .5 + .5;
+    col += doRayShading( light1 , light2 , norm , ro );
+
+  }else{
+
+    col += doBackgroundShading( light1 , light2 , ro );
+
   }
 
-  if( vUv.x < .05 || vUv.x > .95 || vUv.y < .05 || vUv.y > .95 ){
-
-
-    col += doCol( lamb , spec );
-    col += vec3( .3 , .3 , .3 );
-  }
 
   //vec3 col = vec3( 2. - length( texture2D( t_iri , vUv * 4. - vec2( 1.5 ) ) ));
 
@@ -249,6 +428,6 @@ void main(){
   //gl_FragColor = vec4(vec3( length(col)) , 1. );
   gl_FragColor = vec4( col , 1. );
 
-  gl_FragColor = vec4( 1. );
+  //gl_FragColor = vec4( 1. );
 
 }
